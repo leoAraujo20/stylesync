d94bd8d817121b6e33a3559ac890ec6a19a8111e
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-10-02 22:44:52 -0300

    Update Sale schema

--- src/stylesync/schemas/sale.py
+++ src/stylesync/schemas/sale.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import date
 
 class Sale(BaseModel):
     sale_date: date
-    product_id: int
+    product_id: str
     quantity: int
     total_price: float
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
