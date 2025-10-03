cdc87781b235508d6c37a3cd855763b5668043da
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-10-02 22:23:32 -0300

    create UserCreate and UserResponse schemas

--- src/stylesync/schemas/user.py
+++ src/stylesync/schemas/user.py
@@ -1,5 +1,22 @@
+from typing import Optional
 from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import Field, ConfigDict
+
 
 class LoginPayload(BaseModel):
     username: str
     password: str
+
+
+class UserCreate(BaseModel):
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    username: str
+    password: str
+
+    model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
+
+
+class UserResponse(BaseModel):
+    id: str = Field(..., alias="id")
+    username: str
